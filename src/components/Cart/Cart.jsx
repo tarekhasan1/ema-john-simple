15e2cf21a9e5b4ae8a8049e3f52e4e181c623531
Md. Tarek Hasan <tarekhasan.tkg101@gmail.com>
2023-04-23 03:51:18 +0600

    quantity problem solved after some confusing stage

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,9 +7,16 @@ const Cart = (props) => {
     console.log(cart);
     let total = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for (const product of  cart){
-        total = total + product.price;
+        product.quantity = product.quantity || 1;
+        // another way
+        // if(product.quantity === 0){
+        //     product.quantity = 1;
+        // }
+        total = total + product.price* product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
 
     const tax = total*7/100;
@@ -18,7 +25,7 @@ const Cart = (props) => {
     return (
         <div className='cart'>
         <h4>Order Summery</h4>
-        <p>Selected Item: {cart.length}</p>
+        <p>Selected Item: {quantity}</p>
         <p>Total Price: ${total}</p>
         <p>Total Shipping: ${totalShipping}</p>
         <p>Tax: ${tax}</p>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -47,7 +47,20 @@ const Shop = () => {
     },[products]);
 
     const handleAddToCart = (product) => {
-       const newCart = [...cart, product];
+        const newCart = [];
+    //    const newCart = [...cart, product];
+       //if product doesn't exists in the cart, then set the quantity =1
+       //if exists update quantity by 1
+       const exists = cart.find(pd => pd.id ===product.id);
+       if(!exists){
+        product.quantity = 1;
+        newCart = [...cart, product];
+       }
+       else{
+        exists.quantity = exists.quantity + 1;
+        const remaining = cart.filter(pd => pd.id !== product.id);
+        newCart = [...remaining, exists];
+       }
        setCart(newCart);
        addToDb(product.id);
     }
