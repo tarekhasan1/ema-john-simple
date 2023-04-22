9fa6e3bd4e2729eff6316b5a6e9dd14b228f4db3
Md. Tarek Hasan <tarekhasan.tkg101@gmail.com>
2023-04-23 01:10:03 +0600

    added to cart function added and favicon added with title

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css';
 
 const Product = (props) => {
-    console.log(props.product);
     const {img, name, seller, ratings, price} = props.product;
+
+    const handleAddToCart = props.handleAddToCart;
+
     return (
         <div className="product">
         <img src={img} alt="" />
@@ -13,7 +17,9 @@ const Product = (props) => {
         <p>Manufacturer: {seller}</p>
         <p>Ratings: {ratings}</p>
         </div>
-        <button className='btn-cart'>Add to Cart</button>
+        <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>Add to Cart
+        <FontAwesomeIcon icon={faShoppingCart} />
+        </button>
         </div>
     )
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -6,11 +6,20 @@ const Shop = () => {
 
     const [products, setProducts] = useState([]);
 
+    const [cart, setCart] = useState([]);
+
     useEffect(() => {
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data));
     }, []);
+
+    const handleAddToCart = (product) => {
+       const newCart = [...cart, product];
+       setCart(newCart);
+    }
+
+
     return (
         <div className='shop-container'>
         <div className="product-container">
@@ -18,11 +27,13 @@ const Shop = () => {
            products.map(product => <Product
             key={product.id}
             product={product}
+            handleAddToCart={handleAddToCart}
             ></Product>)
         }
         </div>
         <div className="cart-container">
         <h4>Order Summery</h4>
+        <p>Selected Item: {cart.length}</p>
         </div>
         </div>
     );
